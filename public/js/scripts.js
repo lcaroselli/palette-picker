1546688f5208a18342fef13b0cea42da6d987f3a
Laura Caroselli <lcaroselli@outlook.com>
2017-10-05 01:04:57 -0600

    Add POST for projects, set-up save projects functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,7 @@
 const generateColor = () => {
   return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 }
+//Note: this sometimes brings back invalid hex codes
 
 const generatePalette = () => {
   for (let i = 1; i < 6; i++) {
@@ -10,6 +11,41 @@ const generatePalette = () => {
   }
 }
 
+const storeProject = (name) => {
+  fetch('/api/v1/projects', {
+    method: 'POST',
+    body: JSON.stringify({ name }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(response => showProjects(response[0]))
+}
+
+const showProjects = (project) => {
+  const { id, project_name } = project;
+
+  $('#project-list').append(`<option value='${id}'>${project_name}</option>`);
+
+  $('#project-folders-section').append(
+    `<article id='project-${id}'>
+      ${project_name}
+      <img src='../assets/garbage.svg' alt='Delete project icon'>
+    </article>`);
+}
+
+const fetchProjects = () => {
+  fetchAll('projects', showProjects);
+}
+
+const createProject = () => {
+  const name = $('.project-name').val();
+  storeProject(name);
+}
+
 $('#generate-button').on('click', generatePalette);
 
-$(document).ready(generatePalette);
+$('#save-project-button').on('click', createProject);
+
+$(document).ready(generatePalette, fetchProjects);
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
@@ -21,27 +24,37 @@ app.get('/', (request, response) => {
 
 // GET all projects
 app.get('/api/v1/projects', (request, response) => {
-  database('projects').select();
+  database('projects').select()
   .then(projects => {
     if (!projects.length) {
-      return response.status(404).json({ error: 'No projects exist' });
-    } else {
-      response.status(200).json(projects)
-    } });
-  .catch(error => { response.status(500).json({ error }) });
+      return response.status(404).json({ error: 'No projects exist' })
+    } response.status(200).json(projects) })
+  .catch(error => { response.status(500).json({ error }) })
 });
 
 // GET all palettes
 app.get('/api/v1/palettes', (request, response) => {
-  database('palettes').select();
+  database('palettes').select()
   .then(palettes => {
     if (!palettes.length) {
-      return response.status(404).json({ error: 'No palettes exist' });
-    } else {
-      response.status(200).json(palettes)
-    } });
-    .catch(error => { response.status(500).json({ error }) });
+      return response.status(404).json({ error: 'No palettes exist' })
+    } response.status(200).json(palettes) })
+    .catch(error => { response.status(500).json({ error }) })
 });
 
-  //DELETE palette in project folder
-  //DELETE PROJECT FOLDER (with and without palettes)
+// POST project folder
+app.post('/api/v1/projects', (request, response) => {
+  const { project_name } = request.body;
+
+  if (!project_name) {
+    return response.status(422).json({ error: 'Missing property: project_name' })
+  }
+  database('projects').insert({ project_name }, '*')
+  .then(project => response.status(201).json(project))
+  .catch(error => response.status(500).json({ error }))
+});
+
+
+// POST palette
+// DELETE palette
+// DELETE PROJECT FOLDER (with and without palettes)
