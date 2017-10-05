e2a13083e017d87a665610ee6cdbc0a9f227b43b
Laura Caroselli <lcaroselli@outlook.com>
2017-10-04 23:58:05 -0600

    Add GET requests for projects and palettes

--- server.js
+++ server.js
@@ -4,24 +4,44 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.get('/', (request, response) => {
-  response.send('Hello');
-});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
 
-//Endpoints
-  //GET all projects
-  //GET all palettes
+app.get('/', (request, response) => {
+  response.send('Hello');
+});
 
-  //POST new project folder
-  //SAVE(which method ??) palette in project folder
+// GET all projects
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select();
+  .then(projects => {
+    if (!projects.length) {
+      return response.status(404).json({ error: 'No projects exist' });
+    } else {
+      response.status(200).json(projects)
+    } });
+  .catch(error => { response.status(500).json({ error }) });
+});
+
+// GET all palettes
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select();
+  .then(palettes => {
+    if (!palettes.length) {
+      return response.status(404).json({ error: 'No palettes exist' });
+    } else {
+      response.status(200).json(palettes)
+    } });
+    .catch(error => { response.status(500).json({ error }) });
+});
+
   //DELETE palette in project folder
   //DELETE PROJECT FOLDER (with and without palettes)
